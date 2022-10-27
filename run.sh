04b87df8deda611b7aedd3c8bc88c7743a0e7d4f
callbinuvarghese <call.binu.varghese@gmail.com>
2022-10-27 08:54:14 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
+# Run locally, it should connect with public IP enabled on the Cloud SQL Instance. Stop working when public IP is taken off
 mvn spring-boot:run \
   -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=cloud"
+
+# Build it with cloud build
+gcloud builds submit --region=us-east4 --tag us-east4-docker.pkg.dev/$PROJECT_ID/quickstart-docker-repo/api-test-1:latest
+
+# Run it as cloud run
+gcloud run deploy apiserver-test1 \
+    --image=us-east4-docker.pkg.dev/acn-highmark-health-odh/quickstart-docker-repo/api-test-1:latest  \
+    --platform managed \
+    --project=$PROJECT_ID \
+    --region=us-east4 \
+    --set-env-vars "spring.cloud.gcp.sql.instance-connection-name=$PROJECT_ID:us-east4:binu-pg13" \
+    --set-env-vars "spring.cloud.gcp.sql.database-name=test-db"
